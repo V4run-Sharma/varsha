1e7302c320026ad10706e671cf338461580c94a1
Varun <sharmavarun.1912@gmail.com>
2024-07-01 01:49:46 +0530

    feat: Add user wishlist and cart icons to header

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -2,10 +2,11 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="px-60 py-8 w-full flex justify-between items-center bg-neutral-200 dark:bg-neutral-800 text-center">
+    <footer className="px-60 py-8 w-full flex justify-between items-start bg-neutral-100 dark:bg-neutral-900 text-center">
       <div className="flex flex-col items-start">
-        <Link href={"/about/"}>
-          <p className="mb-4 text-3xl font-medium ">About</p>
+        <Link href={"/about"} className="group">
+          <p className="text-3xl font-medium ">About</p>
+          <div className="mb-3 w-0 group-hover:w-2/3 h-[3px] transition-all bg-black dark:bg-white"></div>
         </Link>
         <Link href={"/about/privacy-policy"}>
           <p className="mb-1 text-sm hover:underline">Privacy Policy</p>
@@ -25,8 +26,39 @@ export default function Footer() {
           &copy; 2024 Varsha Watches&trade;. All Rights Reserved.
         </p>
       </div>
-      <div></div>
-      <div></div>
+      <div className="flex flex-col items-start">
+        <Link href={"/stores"} className="group">
+          <p className="text-3xl font-medium ">Stores</p>
+          <div className="mb-3 w-0 group-hover:w-2/3 h-[3px] transition-all bg-black dark:bg-white"></div>
+        </Link>
+        <Link href={"/"}>
+          <p className="mb-1 text-sm hover:underline">Delhi</p>
+        </Link>
+        <Link href={"/"}>
+          <p className="mb-1 text-sm hover:underline">Mumbai</p>
+        </Link>
+        <Link href={"/"}>
+          <p className="mb-1 text-sm hover:underline">Bangalore</p>
+        </Link>
+        <Link href={"/"}>
+          <p className="mb-1 text-sm hover:underline">Kolkata</p>
+        </Link>
+      </div>
+      <div className="flex flex-col items-start">
+        <Link href={"/contact-us"} className="group">
+          <p className="text-3xl font-medium ">Contact Us</p>
+          <div className="mb-3 w-0 group-hover:w-2/3 h-[3px] transition-all bg-black dark:bg-white"></div>
+        </Link>
+        <Link href={"/"}>
+          <p className="mb-1 text-sm hover:underline">Email</p>
+        </Link>
+        <Link href={"/"}>
+          <p className="mb-1 text-sm hover:underline">Phone</p>
+        </Link>
+        <Link href={"/"}>
+          <p className="mb-1 text-sm hover:underline">Address</p>
+        </Link>
+      </div>
     </footer>
   );
 }
--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -5,12 +5,18 @@ import Link from "next/link";
 
 import { ModeToggle } from "@/components/mode-toggle";
 import NavLink from "./navlink";
+import { Heart, ShoppingBag, User } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 const links = [
   { title: "All Watches", href: "/all" },
   { title: "Categories", href: "/categories" },
   { title: "Blogs", href: "/blogs" },
 ];
+
+const cart = 10;
+const wishlist = 5;
+const user = "V";
 
 export default function Header() {
   return (
@@ -29,7 +35,35 @@ export default function Header() {
           <NavLink key={link.title} title={link.title} href={link.href} />
         ))}
       </div>
-      <div>
+      <div className="flex gap-8 justify-center items-center">
+        <Link href={"/userId/wishlist"} className="relative">
+          <Heart size={24} />
+          {wishlist > 0 && (
+            <span className="absolute flex justify-center items-center -top-1 -right-1 w-4 h-4 bg-red-600 text-white text-[10px] font-bold rounded-full">
+              {wishlist}
+            </span>
+          )}
+        </Link>
+        <Link href={"/userId/cart"} className="relative">
+          <ShoppingBag size={24} />
+          {cart > 0 && (
+            <span className="absolute flex justify-center items-center -top-1 -right-1 w-4 h-4 bg-red-600 text-white text-[10px] font-bold rounded-full">
+              {cart}
+            </span>
+          )}
+        </Link>
+        <Link href={"/userId"}>
+          {user ? (
+            <Button
+              variant="outline"
+              size="icon"
+              className="text-lg font-medium">
+              {user}
+            </Button>
+          ) : (
+            <User size={24} />
+          )}
+        </Link>
         <ModeToggle />
       </div>
     </header>
